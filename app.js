2ce7038b54c0518b4a182b174b48328cd8ae208e
kentootz <vquinno@yahoo.com>
2019-05-03 08:54:06 +0800

    trial

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var expect = require('chai').expect
+
+describe("ChaiJs expect", function(){
+	it("ChaiJs expect tutorial", function()	{
+
+		// expect("abc").to.be.a('string');
+		// expect(10).to.be.a('number');
+		// expect(10).to.not.be.a('string');
+		expect("ignition").to.contain('ign')
+	})
+
+	})
